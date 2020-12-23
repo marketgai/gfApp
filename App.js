9328b635af0417c703f0df61c5d0f7f73b0983d0
Marketgai <marcusalee@gmail.com>
2020-12-23 11:19:35 -0800

    fixed async issues with error first callback... might not need awaits

--- App.js
+++ App.js
@@ -7,37 +7,28 @@ import sendSMS from 'react-native-sms';
 import * as SMS from 'expo-sms';
 import axios from 'axios';
 
-
-const getMsg = (group) => {
+const getMsg = (group, callback) => {
   //this is correct endpoint... was working
-  axios.get(`http://54.151.32.166:5555/message/${group}`)
-  .then((response)=> {
-    console.log('response', JSON.stringify(response.data[0].message))
-    return JSON.stringify(response.data[0].message)
-  })
-  .catch((err)=> console.log('error', err))
-}
-
-const messages = ['WHY ARENT YOU PAYING ATTENTION TO ME!!', 'PAY ATTENTION TO ME NOWWW!!', 'HEY WHATS UP', 'I LIKE YOU', 'OKAY NOW IM GETTING ANNOYED', 'I HATE YOU', 'IM SORRY', 'I BOUGHT YOU SOMETHING', 'WANT TO EAT?', 'ARE YOU IGNORING ME?', 'WHAT ARE YOU DOING?', '<3']
-
-const generateMsg = () => {
-  const n = Math.floor(Math.random() * 12);
-  return messages[n];
-}
+  axios
+    .get(`http://54.151.32.166:5555/message/${group}`)
+    .then((response) => {
+      console.log('response', JSON.stringify(response.data[0].message));
+      callback(null, JSON.stringify(response.data[0].message));
+    })
+    .catch((err) => callback(err));
+};
 
 const smsFunc = async () => {
   // alert('ANNOYING BOYFRIEND NOW');
-  const msg = await getMsg('attention');
-  //async issues, msg is undefined
-  console.log(msg)
-  const status = await SMS.sendSMSAsync(
-    ['16505041488'],
-    // generateMsg(),
-    await getMsg('attention')
-
-  );
-  console.log(status);
-}
+  try {
+    const msg = await getMsg('attention', (err, result) => {
+      SMS.sendSMSAsync([ '16505041488' ], result);
+      console.log('result', result);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 class gfApp extends Component {
   render() {
@@ -47,21 +38,26 @@ class gfApp extends Component {
 
         <Text style={{color: '#888', fontSize: 18}}>ATTENTION NOW!!!</Text> */}
         <TouchableOpacity
-          onPress={async () => {
+          onPress={() => {
             // const test = await alert('ANNOYING BOYFRIEND NOW');
-            smsFunc()}
-            // async () => {
-            // alert('ANNOYING BOYFRIEND NOW');
-            // const status = await SMS.sendSMSAsync(
-            //   ['14086366067', '16505041488'],
-            //   'PAY ATTENTION TO ME NOWWWW!!!',
-            // );
-            // console.log(status);
-            // }
+            smsFunc();
           }
-          style={{ backgroundColor: 'white' }}>
-          <Image source={logo} style={{width: 300, height: 300}} />
-          <Text style={{color: '#888', fontSize: 30, alignItems: 'center', justifyContent: 'center' }}>    GET ATTENTION NOW!!!</Text>
+          // async () => {
+          // alert('ANNOYING BOYFRIEND NOW');
+          // const status = await SMS.sendSMSAsync(
+          //   ['14086366067', '16505041488'],
+          //   'PAY ATTENTION TO ME NOWWWW!!!',
+          // );
+          // console.log(status);
+          // }
+          }
+          style={{ backgroundColor: 'white' }}
+        >
+          <Image source={logo} style={{ width: 300, height: 300 }} />
+          <Text style={{ color: '#888', fontSize: 30, alignItems: 'center', justifyContent: 'center' }}>
+            {' '}
+            GET ATTENTION NOW!!!
+          </Text>
         </TouchableOpacity>
         <StatusBar style="auto" />
       </View>
@@ -69,7 +65,7 @@ class gfApp extends Component {
   }
 }
 
-export default gfApp
+export default gfApp;
 
 // const getMsg = (group) => {
 //   axios.get(`${window.location.origin}/message/attention`)
@@ -93,15 +89,13 @@ export default gfApp
 //   console.log(status);
 // }
 
-
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container : {
+    flex            : 1,
+    backgroundColor : '#fff',
+    alignItems      : 'center',
+    justifyContent  : 'center'
+  }
 });
 
 // const { result } = await SMS.sendSMSAsync(
